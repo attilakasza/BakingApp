f9eb7bb8d77eed6f1a76386af6919098092a03d7
Attila <9ttila.kasza@gmail.com>
2018-07-07 06:57:53 +0200

    feat: Add Ingredient class

---
+++ app/src/main/java/com/attilakasza/bakingapp/models/Ingredient.java
@@ -0,0 +1,32 @@
+package com.attilakasza.bakingapp.models;
+
+public class Ingredient {
+
+    private String mQuantity;
+    private String mMeasure;
+    private String mIngredient;
+
+    public String getmQuantity() {
+        return mQuantity;
+    }
+
+    public void setmQuantity(String mQuantity) {
+        this.mQuantity = mQuantity;
+    }
+
+    public String getmMeasure() {
+        return mMeasure;
+    }
+
+    public void setmMeasure(String mMeasure) {
+        this.mMeasure = mMeasure;
+    }
+
+    public String getmIngredient() {
+        return mIngredient;
+    }
+
+    public void setmIngredient(String mIngredient) {
+        this.mIngredient = mIngredient;
+    }
+}
