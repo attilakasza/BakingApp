f71ce7e3b9c328e201e093648aeeb37132acd63a
Attila <9ttila.kasza@gmail.com>
2018-07-07 06:54:09 +0200

    feat: Add Step class

---
+++ app/src/main/java/com/attilakasza/bakingapp/models/Step.java
@@ -0,0 +1,50 @@
+package com.attilakasza.bakingapp.models;
+
+public class Step {
+
+    private int mId;
+    private String mShortDescription;
+    private String mDescription;
+    private String mVideoUrl;
+    private String mThumbnailUrl;
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public String getmShortDescription() {
+        return mShortDescription;
+    }
+
+    public void setmShortDescription(String mShortDescription) {
+        this.mShortDescription = mShortDescription;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public String getmVideoUrl() {
+        return mVideoUrl;
+    }
+
+    public void setmVideoUrl(String mVideoUrl) {
+        this.mVideoUrl = mVideoUrl;
+    }
+
+    public String getmThumbnailUrl() {
+        return mThumbnailUrl;
+    }
+
+    public void setmThumbnailUrl(String mThumbnailUrl) {
+        this.mThumbnailUrl = mThumbnailUrl;
+    }
+}
