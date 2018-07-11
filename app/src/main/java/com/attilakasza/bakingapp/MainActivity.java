324b99858486fe781db84e54120571732b0de14f
Attila <9ttila.kasza@gmail.com>
2018-07-11 10:59:40 +0200

    feat: Update UI of MainActivity

--- app/src/main/java/com/attilakasza/bakingapp/MainActivity.java
+++ app/src/main/java/com/attilakasza/bakingapp/MainActivity.java
@@ -1,13 +1,57 @@
 package com.attilakasza.bakingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.attilakasza.bakingapp.adapters.RecipeAdapter;
+import com.attilakasza.bakingapp.helpers.RecipeService;
+import com.attilakasza.bakingapp.helpers.RetrofitBuilder;
+import com.attilakasza.bakingapp.models.Recipe;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnItemClickListener {
+
+    private ArrayList<Recipe> mRecipes;
+    private RecyclerView mRecyclerView;
+    private RecipeAdapter mRecipeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.rv_recipe);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        RecipeService recipeService = RetrofitBuilder.getRecipeService();
+        final Call<ArrayList<Recipe>> recipe = recipeService.getRecipe();
+
+        recipe.enqueue(new Callback<ArrayList<Recipe>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Recipe>> call, Response<ArrayList<Recipe>> response) {
+
+                mRecipes = response.body();
+                mRecipeAdapter = new RecipeAdapter(mRecipes, MainActivity.this, MainActivity.this);
+                mRecyclerView.setAdapter(mRecipeAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Recipe>> call, Throwable t) {
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(Recipe clickedRecipe) {
+
     }
 }
