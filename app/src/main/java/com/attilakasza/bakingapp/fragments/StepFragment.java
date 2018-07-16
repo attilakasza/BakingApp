01c1f5159f3c7d633ad4e4fdfda6e50b76f59b50
Attila <9ttila.kasza@gmail.com>
2018-07-16 16:50:53 +0200

    feat: Add ExoPlayer

--- app/src/main/java/com/attilakasza/bakingapp/fragments/StepFragment.java
+++ app/src/main/java/com/attilakasza/bakingapp/fragments/StepFragment.java
@@ -1,26 +1,53 @@
 package com.attilakasza.bakingapp.fragments;
 
+import android.app.Dialog;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.attilakasza.bakingapp.R;
 import com.attilakasza.bakingapp.models.Step;
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 import static com.attilakasza.bakingapp.fragments.StepsFragment.RECIPE_STEP;
 
-
 public class StepFragment extends Fragment {
 
+    @BindView(R.id.player_view) SimpleExoPlayerView mExoPlayerView;
+    @BindView(R.id.main_media_frame) FrameLayout mFrame;
     @BindView(R.id.tv_step) TextView mDescription;
+    public static String CURRENT_POSITION = "Current_Position";
     private Step mSteps = new Step();
+    private SimpleExoPlayer mExoPlayer;
+    private BandwidthMeter mBandwidthMeter;
+    private Dialog mFullScreenDialog;
+    private boolean mExoPlayerFullscreen = false;
+    private long mPosition = C.TIME_UNSET;
+    private String mVideoUrl;
+
 
     public StepFragment() {
         // Required empty public constructor
@@ -34,6 +61,7 @@ public class StepFragment extends Fragment {
 
         if(savedInstanceState != null) {
             mSteps = savedInstanceState.getParcelable(RECIPE_STEP);
+            mPosition = savedInstanceState.getLong(CURRENT_POSITION, C.TIME_UNSET);
         }
         else {
             mSteps = getArguments().getParcelable(RECIPE_STEP);
@@ -41,12 +69,88 @@ public class StepFragment extends Fragment {
 
         mDescription.setText(mSteps.getmDescription());
 
+        mVideoUrl = mSteps.getmVideoUrl();
+        if (!mVideoUrl.isEmpty()) {
+            initializeExoPlayer(Uri.parse(mVideoUrl));
+        }
+
         return rootView;
+    }
+
+    private void initializeExoPlayer(Uri uri) {
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(mBandwidthMeter);
+        DefaultTrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+        LoadControl loadControl = new DefaultLoadControl();
+
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
+        mExoPlayerView.setPlayer(mExoPlayer);
+
+        String userAgent = Util.getUserAgent(getContext(), "Baking App");
+        MediaSource mediaSource = new ExtractorMediaSource(uri, new DefaultDataSourceFactory(getContext(),
+                userAgent), new DefaultExtractorsFactory(), null, null);
+        if (mPosition != C.TIME_UNSET) mExoPlayer.seekTo(mPosition);
+        mExoPlayer.prepare(mediaSource);
+        mExoPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mExoPlayer != null) {
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mVideoUrl != null) {
+            if (mExoPlayer == null) {
+                initializeExoPlayer(Uri.parse(mVideoUrl));
+            }
+            if (mPosition != C.TIME_UNSET) mExoPlayer.seekTo(mPosition);
+        }
+        if (!mExoPlayerFullscreen) {
+            mFullScreenDialog = new Dialog(getContext(), android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+            openFullscreenDialog();
+        }
+        if(mExoPlayerFullscreen){
+            closeFullscreenDialog();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mExoPlayer != null) {
+            mPosition = mExoPlayer.getCurrentPosition();
+            mExoPlayer.stop();
+            mExoPlayer.release();
+            mExoPlayer = null;
+        }
+    }
+
+    private void openFullscreenDialog() {
+        ((ViewGroup) mExoPlayerView.getParent()).removeView(mExoPlayerView);
+        mFullScreenDialog.addContentView(mExoPlayerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        mExoPlayerFullscreen = true;
+        mFullScreenDialog.show();
+    }
+
+    private void closeFullscreenDialog() {
+        ((ViewGroup) mExoPlayerView.getParent()).removeView(mExoPlayerView);
+        mFrame.addView(mExoPlayerView);
+        mExoPlayerFullscreen = false;
+        mFullScreenDialog.dismiss();
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putParcelable(RECIPE_STEP, mSteps);
+        outState.putLong(CURRENT_POSITION, mPosition);
     }
 }
