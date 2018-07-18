38f350c9ee1d95af7e9980a1f673abe38c504bbf
Attila <9ttila.kasza@gmail.com>
2018-07-18 19:48:58 +0200

    feat: Implement Master/Detail Flow

--- app/src/main/java/com/attilakasza/bakingapp/activities/DetailActivity.java
+++ app/src/main/java/com/attilakasza/bakingapp/activities/DetailActivity.java
@@ -1,8 +1,12 @@
 package com.attilakasza.bakingapp.activities;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.attilakasza.bakingapp.R;
 import com.attilakasza.bakingapp.adapters.StepAdapter;
@@ -29,6 +33,8 @@ public class DetailActivity extends AppCompatActivity implements StepAdapter.OnI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        checkConnection();
+
         Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -40,8 +46,21 @@ public class DetailActivity extends AppCompatActivity implements StepAdapter.OnI
             fragment.setArguments(selectedRecipeBundle);
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
+                    .replace(R.id.fragment_container_steps, fragment)
                     .commit();
+
+            if (findViewById(R.id.ll_activity_detail).getTag() != null && findViewById(R.id.ll_activity_detail).getTag().equals("tablet")) {
+                final StepsFragment fragment2 = new StepsFragment();
+                Bundle stepBundle = new Bundle();
+                mSteps = new ArrayList<>();
+                mSteps.add(mRecipes.get(0).getmSteps().get(0));
+                stepBundle.putParcelableArrayList(RECIPE_STEP, mSteps);
+                stepBundle.putParcelableArrayList(RECIPE, mRecipes);
+                fragment2.setArguments(stepBundle);
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_step, fragment2)
+                        .commit();
+            }
         } else {
             mRecipes = savedInstanceState.getParcelableArrayList(RECIPE);
             mSteps = savedInstanceState.getParcelableArrayList(RECIPE_STEP);
@@ -66,23 +85,55 @@ public class DetailActivity extends AppCompatActivity implements StepAdapter.OnI
 
     @Override
     public void onClick(Step clickedStep) {
-        final StepsFragment fragment = new StepsFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (checkConnection()) {
+            final StepsFragment fragment = new StepsFragment();
+            FragmentManager fragmentManager = getSupportFragmentManager();
 
-        Bundle stepBundle = new Bundle();
-        mSteps = new ArrayList<>();
-        mSteps.add(clickedStep);
-        stepBundle.putParcelableArrayList(RECIPE_STEP, mSteps);
-        stepBundle.putParcelableArrayList(RECIPE, mRecipes);
-        fragment.setArguments(stepBundle);
+            Bundle stepBundle = new Bundle();
+            mSteps = new ArrayList<>();
+            mSteps.add(clickedStep);
+            stepBundle.putParcelableArrayList(RECIPE_STEP, mSteps);
+            stepBundle.putParcelableArrayList(RECIPE, mRecipes);
+            fragment.setArguments(stepBundle);
 
-        fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, fragment).addToBackStack(BACK_RECIPE_STEP)
-                .commit();
+            if (findViewById(R.id.ll_activity_detail).getTag() != null && findViewById(R.id.ll_activity_detail).getTag().equals("tablet")) {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_step, fragment)
+                        .commit();
+            } else {
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container_steps, fragment).addToBackStack(BACK_RECIPE_STEP)
+                        .commit();
+            }
+        }
     }
 
+    // Method to manually check connection status
+    private boolean checkConnection() {
+        boolean isConnected = ConnectivityReceiver.isConnected();
+        showSnack(isConnected);
+        return  isConnected;
+    }
+
+    // Showing the status in Snackbar
+    private void showSnack(boolean isConnected) {
+        if (!isConnected) {
+            Snackbar snackbar = Snackbar
+                    .make(findViewById(R.id.fragment_container_steps), getResources().getString(R.string.no_internet), Snackbar.LENGTH_LONG);
+
+            View sbView = snackbar.getView();
+            TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(Color.RED);
+            snackbar.show();
+        }
+    }
+
+    /**
+     * Callback will be triggered when there is change in
+     * network connection
+     */
     @Override
     public void onNetworkConnectionChanged(boolean isConnected) {
-
+        showSnack(isConnected);
     }
 }
--- app/src/main/java/com/attilakasza/bakingapp/activities/MainActivity.java
+++ app/src/main/java/com/attilakasza/bakingapp/activities/MainActivity.java
@@ -4,13 +4,18 @@ import android.appwidget.AppWidgetManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import com.attilakasza.bakingapp.R;
 import com.attilakasza.bakingapp.adapters.RecipeAdapter;
+import com.attilakasza.bakingapp.helpers.ConnectivityReceiver;
 import com.attilakasza.bakingapp.helpers.RecipeService;
 import com.attilakasza.bakingapp.helpers.RetrofitBuilder;
 import com.attilakasza.bakingapp.models.Recipe;
@@ -25,7 +30,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnItemClickListener, ConnectivityReceiver.ConnectivityReceiverListener {
 
     @BindView(R.id.rv_recipe) RecyclerView mRecyclerView;
     public static String RECIPE = "RECIPE";
@@ -38,6 +43,8 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnI
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        checkConnection();
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
@@ -63,34 +70,65 @@ public class MainActivity extends AppCompatActivity implements RecipeAdapter.OnI
 
     @Override
     public void onClick(Recipe clickedRecipe) {
-        Bundle bundle = new Bundle();
-        ArrayList<Recipe> recipes = new ArrayList<>();
-        recipes.add(clickedRecipe);
-        bundle.putParcelableArrayList(RECIPE, recipes);
+        if (checkConnection()) {
+            Bundle bundle = new Bundle();
+            ArrayList<Recipe> recipes = new ArrayList<>();
+            recipes.add(clickedRecipe);
+            bundle.putParcelableArrayList(RECIPE, recipes);
 
-        int cakeId = clickedRecipe.getmId();
-        int yellowCakeId = 3;
-        if (cakeId == yellowCakeId) {
-            for (int i = 7; i < 13; i++) {
-                clickedRecipe.getmSteps().get(i).setmId(i);
+            int cakeId = clickedRecipe.getmId();
+            int yellowCakeId = 3;
+            if (cakeId == yellowCakeId) {
+                for (int i = 7; i < 13; i++) {
+                    clickedRecipe.getmSteps().get(i).setmId(i);
+                }
             }
+
+            final Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
+
+            Context context = getApplicationContext();
+            SharedPreferences preferences = context.getSharedPreferences(STORED_RECIPE, Context.MODE_PRIVATE);
+            Gson gson = new Gson();
+            String json = gson.toJson(clickedRecipe);
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(STORED_RECIPE, json);
+            editor.apply();
+
+            Intent updateIntent = new Intent(context, IngredientWidgetProvider.class);
+            updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            context.sendBroadcast(updateIntent);
         }
+    }
 
-        final Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtras(bundle);
-        startActivity(intent);
+    // Method to manually check connection status
+    private boolean checkConnection() {
+        boolean isConnected = ConnectivityReceiver.isConnected();
+        showSnack(isConnected);
+        return isConnected;
+    }
 
-        Context context = getApplicationContext();
-        SharedPreferences preferences = context.getSharedPreferences(STORED_RECIPE, Context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = gson.toJson(clickedRecipe);
+    // Showing the status in Snackbar
+    private void showSnack(boolean isConnected) {
+        if (!isConnected) {
+            Snackbar snackbar = Snackbar
+                    .make(findViewById(R.id.rv_recipe), getResources().getString(R.string.no_internet), Snackbar.LENGTH_LONG);
 
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(STORED_RECIPE, json);
-        editor.apply();
+            View sbView = snackbar.getView();
+            TextView textView = sbView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setTextColor(Color.RED);
+            snackbar.show();
+        }
+    }
 
-        Intent updateIntent = new Intent(context, IngredientWidgetProvider.class);
-        updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-        context.sendBroadcast(updateIntent);
+    /**
+     * Callback will be triggered when there is change in
+     * network connection
+     */
+    @Override
+    public void onNetworkConnectionChanged(boolean isConnected) {
+        showSnack(isConnected);
     }
 }
